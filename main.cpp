f5b13edee003a718514cb3e342a3d6c1c8153874
TheMagickMan <aronw1117@gmail.com>
2018-01-12 15:39:33 +0100

    Laci baszd meg

--- main.cpp
+++ main.cpp
@@ -16,285 +16,298 @@
 
 using namespace std;
 
+struct Node {
+    string gyoker;
+    Node* left;
+    Node* right;
+    Node * parent;
+    Node(string ujgyok) {
+        gyoker = ujgyok;
+        left = NULL;
+        right = NULL;
+    }
+    Node(string ujgyok, Node* szulo) {
+        gyoker = ujgyok;
+        parent = szulo;
+    }
+};
 
-struct darab{
+
+struct darab {
     string nev;
     deque<string> lkod;
     //ha vmi kell meg ide irjatok batran;
 };
 
 
+struct linkedNode {
+    Node *tartalom;
+    linkedNode* kovi;
+};
+
 struct szoveg { //szerintem a structban inkább egy-egy aritmetikai utasításnak kéne csak lenine, nem az egész kódnak
     string fnev;
     vector<darab> komplett;
+    vector<pair<string, Node*> > fak;
     string gkod;
 };
 
-struct Threeadress{
+
+struct Threeadress {
     string baloldal;
     vector<string> jobboldal;
     string operand;
-    Threeadress(){
+    Threeadress() {
         baloldal.clear();
         operand.clear();
     }
 };
 
-struct Node{
-    string gyoker;
-    Node* left;
-    Node* right;
-    Node(string ujgyok){
-        gyoker = ujgyok;
-        left = NULL;
-        right = NULL;
-    }
-    Node(string ujgyok, Node* bal, Node* jobb){
-        gyoker = ujgyok;
-        left = bal;
-        right = jobb;
-    }
-};
 
-struct Tree{
+bool syntaxerror(szoveg &pelda){
+    ifstream f;
+    int i = 0;
+    f.open("proba.txt");
     string nev;
-    stack<Node*> magaafa;
-};
+    string kod;
+    char aktu='.';
+    bool baj;
+    baj=0;
+    while (f.good())
+        {
+            while (aktu!='=')
+            {
+                f >> aktu;
+                if (aktu=='=') break;
+                if (!isupper(aktu))
+                {
+                    cerr << "Error a kifejezesben, rossz karakter:" << aktu << endl;
 
-ostream& operator << (ostream& kimenet, Threeadress& kiirando){
+                    baj=1;
+                }
+
+            }
+            while (aktu!=';')
+            {
+                    f >> aktu;
+                    if (aktu==';') break;
+                    if (isupper(aktu))
+                    {
+                        cerr << "Error az aritmetikai kodban, rossz karakter:" << aktu << endl;
+                    baj=1;
+                    }
+
+            }
+            f >> ws;
+        }
+return(baj);
+}
+//Prajczer Péter látta (15:36)
+
+
+
+void inorder_bejar_kiir(Node* node) {
+    if(node == NULL) {
+        return;
+    }
+    inorder_bejar_kiir(node->left);
+    cout << node->gyoker;
+    inorder_bejar_kiir(node->right);
+
+}
+
+void stackberak(linkedNode **ref_to_top, Node *uj) {
+    linkedNode* newNode;
+    newNode->tartalom = uj;
+    newNode->kovi = *ref_to_top;
+}
+
+bool isempty(linkedNode *top) {
+    if(top==NULL) return 1;
+    else return 0;
+}
+
+bool isoperator(string s) {
+    return(s=="*" || s=="/" || s=="+" || s=="-");
+}
+
+Node* stackbolkivesz(linkedNode** ref_to_top){
+    Node* eredmeny;
+    linkedNode *top;
+    top = *ref_to_top;
+    eredmeny = top->tartalom;
+    *ref_to_top = top->kovi;
+    delete top;
+    return eredmeny;
+}
+
+vector<Threeadress> inorder_threea_nonrec(Node* node) {
+    vector<Threeadress> vissza;
+    Node *current = node;
+    linkedNode *sztekk = NULL;
+    bool vege = 0;
+    while(!vege){
+        if(current != NULL){
+            stackberak(&sztekk, current);
+            current = current->left;
+        }
+        else{
+            if(!isempty(sztekk)){
+                current = stackbolkivesz(&sztekk);
+            }
+        }
+    }
+
+}
+
+ostream& operator << (ostream& kimenet, Threeadress& kiirando) {
     kimenet << kiirando.baloldal << "=";
-    for(int i = 0; i < kiirando.jobboldal.size(); i++){
+    for(int i = 0; i < kiirando.jobboldal.size(); i++) {
         kimenet << kiirando.jobboldal[i];
         if(i == 0) kimenet << kiirando.operand;
     }
 }
 
-void syntaxerror(szoveg &pelda){
-    ifstream f;
-    int i = 0;
-    f.open("proba.txt");
-}
 
-void lengyel (szoveg &pelda){
-    ifstream f,g;
+void lengyel (szoveg &pelda) {
+    fstream f,g,h;
     f.open("proba.txt");
     g.open("proba.txt");
+    h.open("proba.txt");
     string aktu;
+    char aktu2;
+    char kuka;
+    int korr;
+    korr=0;
     string eredet;
-    char szam;
+    string szam;
     darab elso;
     string vissza;
-    stack<char> verem;
-    while (f.good()){
+    vissza="";
+    stack<string> verem;
+    while (f.good()) {
         elso.lkod=deque<string>();
         getline(g,eredet,';');
+        cout << eredet << endl;
         getline(f,aktu,'=');
         elso.nev=aktu;
-        szam='.';
-        while (szam!=';' ){
-            f >> szam;
-            if (szam=='*' || szam=='/' ){
-                while (!verem.empty() && verem.top()!='(' && verem.top()!='+' && verem.top()!='-'){
-                    vissza=verem.top();
-                    elso.lkod.push_back(vissza);
+        cout << aktu<< endl;
+        getline(h,aktu,'=');
+        h >> kuka;
+        vissza=kuka;
+        szam=".";
+        while (szam!=";" ) {
+            while (korr >1&& f.good()) {
+                f >> kuka;
+                korr--;
+            }
+            f >> aktu2;
+            szam=aktu2;
+            if (szam=="*" || szam=="/" ) {
+                while (!verem.empty() && verem.top()!="(" && verem.top()!="+" && verem.top()!="-") {
+                    elso.lkod.push_back(verem.top());
                     verem.pop();
                 }
                 verem.push(szam);
-            }
-            else{
-                if (szam=='+' || szam=='-' ){
-                    while (!verem.empty() && verem.top()!='(' ){
-                        vissza=verem.top();
-                        elso.lkod.push_back(vissza);
+            } else {
+                if (szam=="+" || szam=="-" ) {
+                    while (!verem.empty() && verem.top()!="(" ) {
+                        elso.lkod.push_back(verem.top());
                         verem.pop();
                     }
                     verem.push(szam);
-                }
-                else{
-                    if (szam==')'){
-                        while (verem.top()!='('){
-                            vissza=verem.top();
-                            elso.lkod.push_back(vissza);
+                } else {
+
+                    if (szam==")") {
+                        while (verem.top()!="(") {
+                            elso.lkod.push_back(verem.top());
                             verem.pop();
                         }
+                        h >> kuka;
                         verem.pop();
-                    }
-                    else{
-                        if (szam=='('){
-                            verem.push('(');
-                        }
-                        else{
-                            if (szam==';'){}
-                            else{
+
+                    } else {
+                        if (szam=="(") {
+                            verem.push("(");
+                            h >> kuka;
+                        } else {
+                            if (szam==";") {
+                                ;
+                            } else {
+                                if (korr==1) {
+                                    h >> kuka;
+                                    korr--;
+                                }
                                 vissza=szam;
+                                if (h.good()) {
+                                    h >> aktu2;
+                                    korr++;
+                                }
+                                while (isalnum(aktu2) && h.good()) {
+                                    vissza+=aktu2;
+                                    h >> aktu2;
+                                    korr++;
+                                }
                                 elso.lkod.push_back(vissza);
+                                vissza="";
                             }
                         }
                     }
                 }
             }
-
-  }
-        while (!verem.empty()){
-            vissza=verem.top();
-            elso.lkod.push_back(vissza);
+        }
+        while (!verem.empty()) {
+            elso.lkod.push_back(verem.top());
             verem.pop();
         }
+        cout << endl;
         pelda.komplett.push_back(elso);
         f >> ws;
     }
     /*deque<string> test=pelda.komplett[0].lkod;
-    cout << pelda.komplett[0].nev << "=";
-    while(!test.empty()){
+    while(!test.empty()) {
         cout << test.front();
+        cout << endl;
         test.pop_front();
-    }*/
+        }*/
 }
 
-bool isoperator(string s){
-    return(s=="*" || s=="/" || s=="+" || s=="-");
-}
 
-stack<Node*> expression_tree (deque<string> postfix_code){
-    stack<Node*> uj;
-    while(!postfix_code.empty()){
-        string current = postfix_code.front();
-        if(isoperator(current)){
-            Node* jobb = uj.top();
-            uj.pop();
-            Node* bal = uj.top();
-            uj.pop();
-            Node* temp = new Node(current, bal, jobb);
-            uj.push(temp);
-            delete temp;
-        }
-        else{
-            Node* temp = new Node(current);
-            uj.push(temp);
-            delete temp;
-        }
-        postfix_code.pop_front();
-    }
-    return uj;
-}
 
-vector<Tree> sorok_fai(szoveg pelda){
-    vector<Tree> retval;
-    vector<darab> current = pelda.komplett;
-    for(darab d : current){
-        Tree temp;
-        temp.nev = d.nev;
-        temp.magaafa = expression_tree(d.lkod);
-        retval.push_back(temp);
-    }
-    return retval;
-}
-
-vector<Threeadress> felbont(szoveg pelda){
-    vector<Threeadress> visszateres;
-    for(darab d : pelda.komplett){
-        Threeadress uj;
-        if(uj.baloldal.length()==0 && d.nev.length()!=0){
-            uj.baloldal = d.nev;
-        }
-        while(!d.lkod.empty()){
-            string current = d.lkod.front();
-            if(isalnum(current[0])){
-                if(uj.jobboldal.size()<2){
-                    uj.jobboldal.push_back(current);
-                }
-                else{
-                    Threeadress* masodik = new Threeadress();
-                    masodik->jobboldal.push_back(current);
-                }
-            }
-            else{
-                if(!(uj.operand.length()>0)){
-                    uj.operand = current;
-                }
-                else{
-                    /*if(uj.operand.compare("*")== 0){    //FMA3
-                        if(current=="+"){
-
-                        }
-                    }*/
-                    //visszateres.push_back(uj);
-                    cout << uj << endl;
-                    Threeadress* masodik = new Threeadress;
-                    masodik->baloldal = uj.jobboldal[0]+uj.operand+uj.jobboldal[1];
-                    masodik->operand = current;
-                    uj = *masodik;
-                    delete masodik;
-                    /*uj.baloldal = uj.jobboldal[0]+uj.operand + uj.jobboldal[1];
-                    uj.operand = current;
-                    uj.jobboldal.clear();*/
-                    cout << uj.baloldal;
-                }
-            }
-            d.lkod.pop_front();
-        }
-        cout << uj;
-    }
-    return visszateres;
-}
-
-void regiszteres(szoveg& pelda, int regnum){
-    vector<pair<bool, string> >registers(regnum, make_pair(0, ""));
-    vector<pair<string, pair<char, int> > > valtozok;
-    deque<string> current = pelda.komplett[0].lkod;
-    queue<string> operands;
-    string temp;
-    while(!current.empty()){
-        if(isalnum(current.front()[0])){
-            operands.push(current.front());
-            for(int i = 0; i < registers.size(); i++){
-                if(registers[i].first == 0){
-                    registers[i].first = 1;
-                    registers[i].second = current.front();
-                    temp += "reg["+to_string(i)+"] = "+current.front()+";\n";
-                    current.pop_front();
-                    break;
-                }
+void make_tree(szoveg& pelda) {
+    Node *t, *t1, *t2;
+    for(darab d : pelda.komplett) {
+        stack<Node*> sztekk;
+        deque<string> current = d.lkod;
+        while(!current.empty()) {
+            if(!isoperator(current.front())) {
+                t = new Node(current.front());
+                sztekk.push(t);
+                current.pop_front();
+            } else {
+                t = new Node(current.front());
+                t1 = sztekk.top();
+                sztekk.pop();
+                t2 = sztekk.top();
+                sztekk.pop();
+                t->left = t1;
+                t->right = t2;
+                sztekk.push(t);
+                current.pop_front();
             }
         }
-        else{
-            string op_1 = operands.front();
-            operands.pop();
-            string op_2 = operands.front();
-            operands.pop();
-            for(int i = 0; i < registers.size(); i++){
-                if(registers[i].first == 0){
-                    string ut = "reg["+to_string(i)+ "]= reg["+ to_string(i-2)+"] " +
-                        current.front() + "reg["+to_string(i-1)+"];";
-                    registers[i-1].first = 0;
-                    registers[i-1].second = "";
-                    if(i > 1){
-                        registers[i-2].first = 0;
-                        registers[i-2].second = "";
-                    }
-                    registers[i].first = 1;
-                    registers[i].second = ut;
-                    temp += ut+'\n';
-                    current.pop_front();
-                    operands.push(ut);
-                    break;
-                }
-            }
-        }
+        t = sztekk.top();
+        sztekk.pop();
+        pair<string, Node*> temp = make_pair(d.nev, t);
+        pelda.fak.push_back(temp);
     }
-    for(int i = 0; i < registers.size(); i++){
-        if(registers[i].first == 0){
-            temp += "\n"+pelda.komplett[0].nev + " = reg["+ to_string(i-1)+"];\n";
-            break;
-        }
-    }
-    cout << temp;
 }
 
-int main()
-{
+
+int main() {
     szoveg pelda;
     lengyel(pelda);
-    vector<Tree> valami = sorok_fai(pelda);
+    make_tree(pelda);
+    inorder_bejar_kiir(pelda.fak[0].second);
     return 0;
 }
